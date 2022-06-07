20446d677823db3b6d987503bdfc189c243c427a
LordGhost01 <ghostsmith01@yahoo.com>
2022-06-07 07:00:28 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,47 +1,25 @@
 #include "main.h"
 
-
-
 /**
+ * main - check the code for ALX School students.
  *
- *  * main - check the code for ALX School students.
+ * Program that prints _putchar, followedby a new line
  *
- *   *
- *
- *    * Program that prints _putchar, followed by a new line
- *
- *     *
- *
- *      * Return: always 0 (success)
- *
- *       */
-
-
+ * Return: always 0 (success)
+ */
 
 int main(void)
-
 {
+	char s[] = "_putchar";
+	int i = 0;
 
-		char s[] = "_putchar";
+	while (s[i] != '\0')
+	{
+		_putchar(s[i]);
+		i++;
 
-			int i = 0;
+	}
+	_putchar(10);
 
-
-
-				while (s[i] != '\0')
-
-						{
-
-									_putchar(s[i]);
-
-											i++;
-
-												}
-
-					_putchar(10);
-
-
-
-						return (0);
-
+	return (0);
 }
