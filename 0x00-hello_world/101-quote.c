a89eb3740d8494b657768e2f8841f82a3d041be6
LordGhost01 <ghostsmith01@yahoo.com>
2022-06-07 08:25:43 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
-#include <string.h>
-#include <unistd.h>
 /**
- * main - main block
- * Return: 1
+ *  main - Entry point
+ *
+ *  Return: Always 0 (Success)
  */
 int main(void)
 {
-	int s;
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
 
-	s=strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-
-	write(2,
-			"and that piece of art is useful\" - Dora korpar, 2015-10-19\n",
-			s);
-	return (1);
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+	return (0);
 }
