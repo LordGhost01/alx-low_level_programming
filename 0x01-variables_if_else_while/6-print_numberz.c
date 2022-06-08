79c91feee918d95137e5e2ba8162056a3c401d22
LordGhost01 <ghostsmith01@yahoo.com>
2022-06-08 03:11:29 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -5,14 +5,16 @@
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	int i;
 
-	for (i = o; i <  10; i++)
+	for (i = '0'; i <= '9'; i++)
 	{
-		putchar(i + 48);
+		putchar(i);
 	}
-	putchar(10);
+	putchar('\n');
+
 	return (0);
 }
