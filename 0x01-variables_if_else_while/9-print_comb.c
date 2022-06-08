3f57e79ef33b95d834b9534895fe5bb35d7182ec
LordGhost01 <ghostsmith01@yahoo.com>
2022-06-08 04:12:19 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,13 +9,12 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i <= 9; i++)
+	for (i = '0'; i <= '9'; i++)
 	{
 		putchar(i);
-		
-		if (i != 9)
+		if (i != '9')
 		{
-			putchar(, );
+			putchar(', ');
 		}
 	}
 	putchar('\n');
