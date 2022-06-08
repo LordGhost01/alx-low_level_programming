38a123b5641c50da703d6a89898110650ead60ba
LordGhost01 <ghostsmith01@yahoo.com>
2022-06-08 06:03:37 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -20,11 +20,11 @@ int main(void)
 					{
 						if (((k + j) > (c + i) &&  k >= c) || c < k)
 						{
-						putchar(c);
-						putchar(i);
-						putchar(' ');
-						putchar(k);
-						putchar(j);
+							putchar(c);
+							putchar(i);
+							putchar(' ');
+							putchar(k);
+							putchar(j);
 							if (c + i + k + j == 227 && c == 57)
 							{
 							break;
