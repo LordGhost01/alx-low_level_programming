bd56b11f95d4c2e60e163008ee5cb944117dfb62
LordGhost01 <ghostsmith01@yahoo.com>
2022-06-08 04:07:35 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Prins a series of numbers with commas
+ * Return: Always (Success);
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar(i);
+		
+		if (i != 9)
+		{
+			putchar(, );
+		}
+	}
+	putchar('\n');
+	return (0);
+}
