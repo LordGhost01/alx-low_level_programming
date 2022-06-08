aaac05986e5dcd642466c5cea8185742eeed24d0
LordGhost01 <ghostsmith01@yahoo.com>
2022-06-08 08:30:14 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,29 +10,29 @@ void print_times_table(int n)
 {
 		if (n >= 0 && n <= 15)
 		{
-		for (num = 0; num <= n; num++)
+			for (num = 0; num <= n; num++)
 			{
 				_putchar('0');
-				for (mult = 1; mult <= n; mult++)
-				{
-					_putchar(',');
+			for (mult = 1; mult <= n; mult++)
+			{
+				_putchar(',');
+				_putchar(' ');
+				prod = num * mult;
+				if (prod <= 99)
+				if (prod <= 99)
+				if (prod <= 9)
 					_putchar(' ');
-					prod = num * mult;
-					if (prod <= 99)
-						if (prod <= 99)
-							if (prod <= 9)
-								_putchar(' ');
-					if (prod >= 100)
-					{
-						_putchar((prod / 100) + '0');
-						_putchar(((prod / 10)) % 10 + '0');
-					}
-					else if (prod <= 99 && prod >= 10)
-					{
-						_putchar((prod / 10) + '0');
-					}
-					_putchar((prod % 10) + '0');
+				if (prod >= 100)
+				{
+					_putchar((prod / 100) + '0');
+					_putchar(((prod / 10)) % 10 + '0');
 				}
+				else if (prod <= 99 && prod >= 10)
+				{
+						_putchar((prod / 10) + '0')
+				}
+				_putchar((prod % 10) + '0');
+			}
 				_putchar('\n');
 			}
 		}
