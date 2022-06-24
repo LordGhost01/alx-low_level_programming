87ccd30c61853f3b65e110f6f9bce4d659d09cbd
AnnGilbert <queengilbert405@gmail.com>
2022-06-24 13:21:24 -0700

    script of  a program that prints the minimum number of coins to make change for an amount of money

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,38 +1,53 @@
+
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 
 /**
- * main - prints the minimum number of coins to make change for an amount.
- * of money.
- * @argc: number of command line arguments.
- * @argv: array that contains the program command line arguments.
- * Return: 0 - success.
+ * main - print the min number of coins to make change for an amount of money
+ * @argc: argument count
+ * @argv: argument vector, array of strings
+ * Return: 1 if error, 0 otherwise
  */
 
 int main(int argc, char *argv[])
 {
-	int cents, ncoins = 0;
+	int total, count;
+	unsigned int i;
+	char *p;
+	int cents[] = {25, 10, 5, 2};
 
-	if (argc == 1 || argc > 2)
+	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	cents = atoi(argv[1]);
-	while (cents > 0)
+
+	total = strtol(argv[1], &p, 10);
+	count = 0;
+
+	if (!*p)
 	{
-	if (cents >= 25)
-		cents -= 25;
-	else if (cents >= 10)
-		cents -= 10;
-	else if (cents >= 5)
-		cents -= 5;
-	else if (cents >= 2)
-		cents -= 2;
-	else if (cents >= 1)
-		cents -= 1;
-		ncoins += 1;
+		while (total > 1)
+		{
+			for (i = 0; i < sizeof(cents[i]); i++)
+			{
+				if (total >= cents[i])
+				{
+					count += total / cents[i];
+					total = total % cents[i];
+				}
+			}
+		}
+		if (total == 1)
+			count++;
 	}
-	printf("%d\n", ncoins);
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	printf("%d\n", count);
 	return (0);
 }
