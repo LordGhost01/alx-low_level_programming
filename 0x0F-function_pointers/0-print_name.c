b113ec6b2eac93bde2ce20d693594dc8a92eb405
AnnGilbert <queengilbert405@gmail.com>
2022-07-07 09:27:18 -0700

    script that prints a name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,15 +1,14 @@
+#include <stdlib.h>
 #include "function_pointers.h"
 
 /**
- * print_name - prints a name.
- * @name: input name.
- * @f: function pointer.
- *
- * Return: no return.
+ * print_name - prints a name
+ * @name: name to print
+ * @f: pointer to the printing function
  */
-
 void print_name(char *name, void (*f)(char *))
 {
-	if (f && name)
-		(*f)(name);
+	if (!name || !f)
+		return;
+	f(name);
 }
